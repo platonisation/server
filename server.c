1a1e6b0d89c3a8d4503a080bd525186498f92c42
tbn <tbn@T-BONNIN>
2014-04-24 15:17:47 +0200

    communication by submodules

--- server.c
+++ server.c
@@ -24,26 +24,22 @@ autre utilisateur
  *
  */
 
-#include <sys/socket.h>       /*  socket definitions        */
-#include <sys/types.h>        /*  socket types              */
-#include <arpa/inet.h>        /*  inet (3) funtions         */
-#include <unistd.h>           /*  misc. UNIX functions      */
-#include <errno.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "dependencies/communication/communication.h"
+
+#define debugTrace(string) printf("\n%s\n",string);
 
 typedef struct globalData {
-    int*       	socketFd;               /*  connection sockets        */
-    int       	mainSocket;                 /*  listening socket          */
+    int       	socketFd;               /*  connection sockets        */
+    int       	mainSocket;             /*  listening socket          */
     short int 	port;                   /*  port number               */
     struct    	sockaddr_in servaddr;	/*  socket address structure  */
     char*		messageToSend;			/* message from server to client*/
     char*		messageToReceive;		/* message from server to client*/
 
-}context;
+}contextServer;
 
 //pour les tests. a changer
-context ctx;
+contextServer ctx;
 
 /*  Global constants  */
 
@@ -51,11 +47,11 @@ context ctx;
 #define MAX_LINE           (1000)
 #define LISTENQ				100
 
-int Readline(int sockd, char* buffer, size_t maxlen);
+//int Readline(int sockd, char* buffer, size_t maxlen);
 void doAction(char* buffer, int size);
 void killsrv(int socketFd);
-ssize_t Writeline(int sockd, const void *vptr, size_t n);
-char* parseMessage(char* buffer, int size) ;
+//ssize_t Writeline(int sockd, const void *vptr, size_t n);
+//char* parseMessage(char* buffer, int size) ;
 
 int main(int argc, char *argv[]) {
 
@@ -70,17 +66,17 @@ int main(int argc, char *argv[]) {
 
     if ( argc == 2 ) {
 	ctx.port = strtol(argv[1], &endptr, 0);
-	if ( *endptr ) {
-	    fprintf(stderr, "ECHOSERV: Invalid port number.\n");
-	    exit(EXIT_FAILURE);
-	}
+		if ( *endptr ) {
+			fprintf(stderr, "ECHOSERV: Invalid port number.\n");
+			exit(EXIT_FAILURE);
+		}
     }
     else if ( argc < 2 ) {
-	ctx.port = ECHO_PORT;
+    	ctx.port = ECHO_PORT;
     }
     else {
-	fprintf(stderr, "ECHOSERV: Invalid arguments.\n");
-	exit(EXIT_FAILURE);
+		fprintf(stderr, "ECHOSERV: Invalid arguments.\n");
+		exit(EXIT_FAILURE);
     }
 
 
@@ -125,18 +121,20 @@ int main(int argc, char *argv[]) {
     	ret = select(FD_SETSIZE,&read_selector,(fd_set *)NULL,(fd_set *)NULL,NULL);
     	if(ret > 0){
 			if(FD_ISSET(ctx.mainSocket,&read_selector)){ //main socket
+				debugTrace("ISSET");
+				printf("ISSET\n");
 				/*  Wait for a connection, then accept() it  */
-				int sock = accept(ctx.mainSocket, NULL, NULL);
+				ctx.socketFd = accept(ctx.mainSocket, NULL, NULL);
 	//    		if ( (ctx.socketFd = accept(ctx.mainSocket, NULL, NULL) ) < 0 ) {
-				if(sock == -1) {
+				if(ctx.socketFd == -1) {
 					fprintf(stderr, "ECHOSERV: Error calling accept()\n%s", strerror(errno));
 					killsrv(ctx.socketFd);
 					exit(EXIT_FAILURE);
 				}
-				FD_SET(sock,&read_selector);
+				FD_SET(ctx.socketFd,&read_selector);
+				//stocker les differents FD
 			}
 			else{//client
-
 				Readline(ctx.socketFd, ctx.messageToReceive, MAX_LINE-1);
 				ctx.messageToSend = parseMessage(buffer,strlen(buffer));
 				Writeline(ctx.socketFd, ctx.messageToSend, strlen(ctx.messageToSend));
@@ -149,12 +147,12 @@ int main(int argc, char *argv[]) {
 					fprintf(stderr, "ECHOSERV: Error calling close()\n");
 					exit(EXIT_FAILURE);
 	//				}
-					printf("LE CLIENT IS DEAD !\n");
+					debugTrace("LE CLIENT IS DEAD !\n");
 				}
 			}
     	}
     	else{
-    		printf("toto\n");
+    		debugTrace("toto");
     	}
     }
 }
@@ -185,109 +183,10 @@ void doAction(char* buffer,int size) {
 }
 
 
-/*  Write a line to a socket  */
-
-ssize_t Writeline(int sockd, const void *vptr, size_t n) {
-    size_t      nleft;
-    ssize_t     nwritten;
-    const char *buffer;
-
-    buffer = vptr;
-    nleft  = n;
-
-    while ( nleft > 0 ) {
-	if ( (nwritten = write(sockd, buffer, nleft)) <= 0 ) {
-	    if ( errno == EINTR )
-		nwritten = 0;
-	    else
-		return -1;
-	}
-	nleft  -= nwritten;
-	buffer += nwritten;
-    }
-
-    return n;
-}
-
 void killsrv(int socketFd){
 	if ( close(socketFd) < 0 ) {
 		    fprintf(stderr, "ECHOSERV: Error calling close()\n");
 		    exit(EXIT_FAILURE);
 	}
-}
-
-/*  Read a line from a socket  */
-
-int Readline(int sockd, char* buffer, size_t maxlen) {
-
-	//data to read
-	unsigned char start;
-	unsigned char src[4];
-	unsigned char dst[4];
-	unsigned char size;
-	unsigned char data[20];
-
-	//read selection
-	fd_set read_selector;
-	//timeout of read
-	struct timeval timeout;
-	timeout.tv_sec = 30;
-	timeout.tv_usec = 0;
-	//return value
-	int retval;
-	//init read selection
-	FD_ZERO(&read_selector);
-	FD_SET(sockd,&read_selector);
-	retval = select(sockd+1,&read_selector,NULL,NULL,NULL);
-	if(retval) {
-		//treat data
-		read(sockd, &start, 1);
-		if (start == 0xFE) {
-//			read(sockd,src,4);
-//			read(sockd,dst,4);
-			read(sockd,&size,1);
-			printf("size:%d\n",size);
-			//size in bytes
-			if(size < 10000000){//10Mo, msg
-				if((read(sockd,buffer,size) != size)){
-					//Lecture ok
-					printf("Cannot read %d datas\n",size);
-				}
-				else {
-					printf("%s",buffer);
-					doAction(buffer,size);
-				}
-
-			}
-			else { // files
-				//attention bug ! client envoie coucou trouve un fichier
-				printf("This is a file\n");
-			}
-		}
-		else {
-			printf("Not a starting sequence\n");
-		}
-	}
-	else if(retval == -1){
-		//treat error
-		printf("RETVAL==1\n");
-	}
-	else{
-//		ctx.endConnection = 1;
-		//treat no data found
-		printf("NODATA?\n");
-	}
-
-	return 1;
-}
-
-char* parseMessage(char* buffer, int size) {
-	char* s;
-	s = malloc(size*sizeof(char) + 2);
-	s[0]=0xFE;
-	s[1]=strlen(buffer);
-	strcat(s,buffer);
-
-	return s;
 }
 
